23c3ff551e000e88ab2e690469edcd4a0e001ff4
Teifion <teifion@teifion.co.uk>
2022-03-09 22:06:00 +0000

    Tweaks

--- empty object/cls_view.ex
+++ empty object/cls_view.ex
@@ -2,7 +2,7 @@ defmodule AppnameWeb.Modulename.ClassnameView do
   use AppnameWeb, :view
 
   @spec view_colour() :: atom
-  def view_colour, do: Appname.Modulename.ClassnameLib.colours()
+  def view_colour, do: Appname.Modulename.ClassnameLib.colour()
 
   @spec icon() :: String.t()
   def icon, do: Appname.Modulename.ClassnameLib.icon()
